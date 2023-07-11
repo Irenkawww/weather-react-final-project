2ec11deea35a56afbce11d114e694edafc79c9e8
Irenwww <irenka.www@gmail.com>
2023-07-11 20:55:46 +0100

    added CSS

--- src/Weather.js
+++ src/Weather.js
@@ -8,10 +8,10 @@ export default function Weather(){
         <form>
         <div className='row'>
             <div className='col-9'>
-                <input type='search' className='form-control' placeholder='Enter a city...'/>
+                <input type='search' className='form-control' placeholder='Enter a city...' autoFocus="on"  />
             </div>
         <div className='col-3'>
-            <input type='submit' value='Search' className='btn btn-primary' />
+            <input type='submit' value='Search' className='btn btn-primary w-100 buttonstyle' />
         </div>
      </div>
      </form>
@@ -21,11 +21,14 @@ export default function Weather(){
         <li>Clear Sky</li>
      </ul>
       <div className='row'>
-        <div className='col-6'>
-         <img src='https://github.com/Irenkawww/the-weather-app/blob/main/media/weather_2.jpg?raw=true' />
-          20 °C |°F
+        <div className='col-7'>
+         <img src='https://github.com/Irenkawww/the-weather-app/blob/main/media/weather_2.jpg?raw=true' 
+         alt='weather' 
+         width='35%' />
+          <span className='temprature'>20</span> 
+          <span className='unit'>°C |°F </span>
         </div>
-        <div className='col-6'>
+        <div className='col-5'>
             <ul>
              <li>Humidity: 62%</li>
              <li>Wind: 5 Km/H</li>
