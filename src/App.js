1441d6f6410a7e65a2c3f06b66e53934582b7204
Irenwww <irenka.www@gmail.com>
2023-07-11 11:48:21 +0100

    clean up unnecessary code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     My Weather App
+     <footer><a href="https://github.com/Irenkawww/weather-react-final-project" target='blank'>Open-sourse coded</a> by Iryna Shcherbytska</footer>
     </div>
   );
 }
 
-export default App;
+
