b598a34fa1970129cbdaa0d7e89dfe37ae869b98
Irenwww <irenka.www@gmail.com>
2023-07-11 12:18:07 +0100

    instal Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ export default function App() {
   return (
     <div className="App">
      My Weather App
-     <footer><a href="https://github.com/Irenkawww/weather-react-final-project" target='blank'>Open-sourse coded</a> by Iryna Shcherbytska</footer>
+     <button className='btn btn-primary'>Hello</button>
+     <footer><a href="https://github.com/Irenkawww/weather-react-final-project" target='blank'>Open-sourse coded</a> by <a href="https://portfolio-project-2023.netlify.app/" target='blank'>Iryna Shcherbytska</a></footer>
     </div>
   );
 }
